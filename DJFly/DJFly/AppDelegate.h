526109282192c9e8cff1c1be6f2477f362ab3539
Michael Cheng <mikehcheng@berkeley.edu>
2014-03-28 21:22:12 -0400

    Project started ^_^

---
+++ DJFly/DJFly/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DJFly
+//
+//  Created by Michael Cheng on 3/28/14.
+//  Copyright (c) 2014 Michael Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DJFly/DJFly/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DJFly
+//
+//  Created by Michael Cheng on 3/28/14.
+//  Copyright (c) 2014 Michael Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
