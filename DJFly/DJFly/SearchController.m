4b02505d3103fd93d18d67161b3bf48d02c9884b
chongzluong <timothy.chongz.luong@gmail.com>
2014-03-30 02:11:09 -0400

    Search works, UITable isn't filled yet

--- DJFly/DJFly/SearchController.m
+++ DJFly/DJFly/SearchController.m
@@ -57,24 +57,45 @@
     return [trackCount intValue];
 }
 
+/*
+- (UITableViewCell *)tableView:(UITableView *)aTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *DetailCellIdentifier = @"DetailCell";
+    
+    UITableViewCell *cell = [aTableView dequeueReusableCellWithIdentifier:DetailCellIdentifier];
+    
+    if (cell == nil) {
+        NSArray *cellObjects = [[NSBundle mainBundle] loadNibNamed:@"DetailCell" owner:self options:nil];
+        cell = (UITableViewCell*) [cellObjects objectAtIndex:0];
+    }
+    
+    // setup your cell
+}
+*/
+
 -(BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString {
     // Tells the table data source to reload when text changes
     //[self filterContentForSearchText:searchString scope:
-    NSDictionary *dic = @{@"query" : searchString, @"object" : @"Track"};
-    [_sharedrdio callAPIMethod:@"search" withParameters:dic delegate: self];
+    NSDictionary *dic = @{@"query" : searchString, @"types" : @"Track"};
+    if (searchString.length>3) {
+        [_sharedrdio callAPIMethod:@"search" withParameters:dic delegate: self];
     
-     [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]];
+        [[self.searchDisplayController.searchBar scopeButtonTitles] objectAtIndex:[self.searchDisplayController.searchBar selectedScopeButtonIndex]];
     // Return YES to cause the search result table view to be reloaded.
+    }
     return YES;
 }
 
 
--(void) getRequest: (RDAPIRequest *)request didLoadData:(id) data {
+-(void) rdioRequest: (RDAPIRequest *)request didLoadData:(id) data {
     NSDictionary *tempDic = (NSDictionary *) data;
-    NSNumber *trackCount = [tempDic objectForKey:@"track_count"];
+    trackCount = [tempDic objectForKey:@"track_count"];
+    if ([trackCount intValue] > 10) {
+        trackCount = [NSNumber numberWithInteger:10];
+    }
     NSArray *overallResults = [tempDic objectForKey:@"results"];
-    NSMutableArray *dictionaryArray = [[NSMutableArray init] alloc];
-    
+    NSMutableArray *dictionaryArray = [[NSMutableArray alloc] init];
+
     for (int x = 0; x < [trackCount intValue]; x++){
         
         NSDictionary *results = [overallResults objectAtIndex:x];
@@ -87,7 +108,7 @@
     
 }
 
--(void) getRequest: (RDAPIRequest *) request didFailWithError:(NSError *) error {
+-(void) rdioRequest: (RDAPIRequest *) request didFailWithError:(NSError *) error {
     NSLog(error);
 }
 
